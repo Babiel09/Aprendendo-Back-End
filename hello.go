7c550390187bb50afeaef9d1942ecdcd129a7ea8
Gabriel Castro <estudosbabiel@gmail.com>
2024-10-06 19:35:08 -0300

    Aprendendo o que é o scan

--- hello.go
+++ hello.go
@@ -1,9 +1,14 @@
 package main
 
-import "reflect"
+import (
+	"fmt"
+)
 
 func main() {
-	println("Mascote fofo demais")
-	var nome string = "Teste"
-	println(nome, reflect.TypeOf(nome))
+	fmt.Println("1- Inicar o monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
 }
