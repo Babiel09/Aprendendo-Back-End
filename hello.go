999d4cd7832c657495401fef68cf800e241d5173
Gabriel Castro <estudosbabiel@gmail.com>
2024-10-06 20:38:08 -0300

    Adicionando algumas funções para incurtar a função principal

--- hello.go
+++ hello.go
@@ -2,13 +2,40 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("1- Inicar o monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do programa")
+	perguntarComando()
+	comando := escanearComando()
 
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 3:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+
+	default:
+		fmt.Println("Não conheço este comando")
+		fmt.Println("Saindo do programa...")
+	}
+}
+
+// Criando novas funções
+// Função para perguntar o quê o usuário vai fazer
+func perguntarComando() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+	return
+}
+
+// Função para escanear o comando:
+func escanearComando() int {
 	var comando int
 	fmt.Scan(&comando)
+	return comando //Aqui é obrigatório eu retornar o comando sendo que eu declarei ele lá em cima
 }
