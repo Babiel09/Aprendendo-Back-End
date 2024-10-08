9ea88430b390d03f24d7656c89c6110f85360eff
Gabriel Castro <estudosbabiel@gmail.com>
2024-10-07 22:15:11 -0300

    Deixando minha aplicação cada vez melhor

--- hello.go
+++ hello.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
+	"fmt"      //FMT é o pacote básico do GOLang
+	"net/http" //O HTTP é para eu fazer requisições WEB
+	"os"       //O OS serve para eu fazer algo em relação ao sistema operacional do usuário
+	"time"
 )
 
+// Defino essas variáveis globalmente pois elas serão usadas muitas vezes:
+var siteses = []string{""}
+var site string
+
 func main() {
+
 	for {
 		perguntarComando()
 		comando := escanearComando()
@@ -16,6 +22,8 @@ func main() {
 			monitorarSite()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+		case 3:
+			repetirMonitoramento()
 		case 0:
 			fmt.Println("Ok, até mais")
 			os.Exit(0)
@@ -31,6 +39,7 @@ func main() {
 func perguntarComando() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
+	fmt.Println("3- Teste mais robusto")
 	fmt.Println("0- Sair do Programa")
 	return
 }
@@ -44,16 +53,35 @@ func escanearComando() int {
 
 // Criando a função para monitorar o site:
 func monitorarSite() {
+
 	fmt.Println("Digite a URL do site para monitoramento:")
-	var site string
+
 	//Pedir para o usuário escrever o site que deseja monitorar!
 	fmt.Scan(&site)
+	siteses = append(siteses, site)
 	fmt.Println("Monitorando...")
 	res, _ := http.Get(site)
 	if res.StatusCode == 200 {
-		fmt.Println("O site ", site, "' está no ar")
+		fmt.Println("O site *", site, "* está no ar")
 	} else {
 		fmt.Println("Deu um erro absurdo, verifique se o site está no ar ou se ele existe!")
 		fmt.Println(res.StatusCode)
+	}
+
+	fmt.Println("Você fez um monitoramento desses sites: *", siteses, "*")
+
+}
+func repetirMonitoramento() {
+	monitorarSite()
+	for {
+		time.Sleep(4 * time.Second)
+		fmt.Println("Monitorando...")
+		res, _ := http.Get(site)
+		if res.StatusCode == 200 {
+			fmt.Println("O site *", site, "* está no ar")
+		} else {
+			fmt.Println("Deu um erro absurdo, verifique se o site está no ar ou se ele existe!")
+			fmt.Println(res.StatusCode)
+		}
 	}
 }
