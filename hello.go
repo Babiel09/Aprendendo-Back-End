d7489d16b2f85afb88781296d749ca7399defb5b
Gabriel Castro <estudosbabiel@gmail.com>
2024-10-09 21:43:57 -0300

    Aplicação finalizada

--- hello.go
+++ hello.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"      //FMT é o pacote básico do GOLang
+	"fmt" //FMT é o pacote básico do GOLang
+	"io/ioutil"
 	"net/http" //O HTTP é para eu fazer requisições WEB
 	"os"       //O OS serve para eu fazer algo em relação ao sistema operacional do usuário
 	"strconv"
@@ -24,6 +25,8 @@ func main() {
 		case 1:
 			monitorarSite()
 		case 2:
+			mostrarOsLogs()
+		case 3:
 			repetirMonitoramento()
 		case 0:
 			fmt.Println("Ok, até mais")
@@ -39,7 +42,8 @@ func main() {
 // Função para perguntar o quê o usuário vai fazer
 func perguntarComando() {
 	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Monitoramento mais robusto")
+	fmt.Println("2- Mostrar Logs localmente")
+	fmt.Println("3- Monitoramento mais robusto")
 	fmt.Println("0- Sair do Programa")
 	return
 }
@@ -65,7 +69,7 @@ func monitorarSite() {
 
 	fmt.Println("Você fez um monitoramento desse(s) site(s): *", siteses, "*")
 	fucaoDeLog(site, true)
-	fmt.Println("Todos os sites que foram monitorados estão no arquivo 'log.txt'")
+	fmt.Println("Todos os sites que foram monitorados estão no arquivo: 'log.txt'")
 	if err != nil {
 		fmt.Println("Ocorreu um erro, virifique a url do site:", err, res)
 	}
@@ -97,6 +101,15 @@ func fucaoDeLog(site string, online bool) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	arquivo.WriteString(site + " *site online: " + strconv.FormatBool(online) + "* " + "\n") //uso o "\n" para pular uma linha
+	arquivo.WriteString(time.Now().Format("02-01-2006 15:04:05") + " " + site + " *site online: " + strconv.FormatBool(online) + "* " + "\n") //uso o "\n" para pular uma linha
 	arquivo.Close()
+}
+
+// Criando a função para mostrar os logs:
+func mostrarOsLogs() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(arquivo))
 }
